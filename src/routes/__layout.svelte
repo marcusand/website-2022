a324920b68e0c72fafcdb3c295032539ab98d279
Marcus Schreiter <marcus.schreiter@meso.design>
2022-01-23 22:41:07 +0100

    add basic layout and routes

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,29 @@
+<svelte:head>
+	<title>marcusand</title>
+	<meta name="description" content="personal website of marcusand" />
+	<meta
+		name="keywords"
+		content="marcusand, marcus, software, web, development, personal, website"
+	/>
+</svelte:head>
+
+<main>
+	<slot />
+</main>
+
+<style>
+	:global(html),
+	:global(body),
+	:global(#svelte) {
+		margin: 0;
+		padding: 10;
+	}
+
+	:global(*) {
+		box-sizing: border-box;
+	}
+
+	main {
+		padding: 1rem;
+	}
+</style>
