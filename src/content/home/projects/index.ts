dc1180a7fc6de1bc604830e93d80d98696a5ce57
Marcus Schreiter <mail@marcusand.de>
2022-05-16 10:20:25 +0200

    add fhg project

--- src/content/home/projects/index.ts
+++ src/content/home/projects/index.ts
@@ -2,8 +2,9 @@ import drm from '$content/home/projects/drm.yaml';
 import recherchestation from '$content/home/projects/recherchestation.yaml';
 import dialogmuseum from '$content/home/projects/dialogmuseum.yaml';
 import kvwl from '$content/home/projects/kvwl.yaml';
+import fhg from '$content/home/projects/fhg.yaml';
 
 import metronome from '$content/home/projects/metronome.yaml';
 import sloepaul from '$content/home/projects/sloepaul.yaml';
 
-export default [drm, recherchestation, dialogmuseum, kvwl, metronome, sloepaul];
+export default [fhg, drm, recherchestation, dialogmuseum, kvwl, metronome, sloepaul];
