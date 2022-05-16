280e73c4cead5e6d5a0b9378b9f81b6125dc27bb
Marcus Schreiter <mail@marcusand.de>
2022-05-16 09:46:37 +0200

    add deploy configuration

--- svelte.config.js
+++ svelte.config.js
@@ -3,11 +3,20 @@ import yaml from '@rollup/plugin-yaml';
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: preprocess(),
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      pages: './deploy/build'
+    }),
+    trailingSlash: 'always',
+    prerender: {
+      default: true,
+      crawl: true,
+      enabled: true,
+      onError: 'continue',
+      entries: ['*']
+    },
     vite: {
       plugins: [yaml()],
       resolve: {
