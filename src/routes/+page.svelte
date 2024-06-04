94df5976caf9d7ab847b2c593913e56649568e34
Marcus Schreiter <mail@marcusand.de>
2024-06-04 16:29:48 +0200

    update front page text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,14 +4,15 @@
   const { location } = personData;
 </script>
 
-<h1>Hey</h1>
+<h1>Hey!</h1>
 <p>
   I'm Marcus, a software developer specialized in frontend development, user interfaces and
   multimedia technology.
 </p>
 <p>
   For the last few years I have developed web sites, web apps, a chrome extension, interactive
-  exhibits and backend systems for museums and showrooms. If you want to learn more about that check
-  <a href="/projects">my projects</a>.
+  exhibits and backend systems for museums and showrooms. If you want to learn more about that make
+  sure to check the
+  <a href="/projects">projects page</a>.
 </p>
 <p>I'm currently based in {location}.</p>
