60a829fb72ee65930ce9e065262212ef4ec0dfea
Marcus Schreiter <mail@marcusand.de>
2024-06-11 18:24:59 +0200

    update content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,9 +10,12 @@
   multimedia technology.
 </p>
 <p>
-  For the last few years I have developed web sites, web apps, a chrome extension, interactive
+  For the last couple of years I have developed web sites, web apps, a chrome extension, interactive
   exhibits and backend systems for museums and showrooms. If you want to learn more about that make
   sure to check the
   <a href="/projects">projects page</a>.
 </p>
-<p>I'm currently based in {location}.</p>
+<p>
+  I'm currently based in {location} and looking for a new job — feel free to
+  <a href="/contact">contact me</a>.
+</p>
