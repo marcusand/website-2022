36d0921f239655ffe80d23448a9db5b1cf8016fa
Marcus Schreiter <mail@marcusand.de>
2024-06-11 18:14:51 +0200

    add paragraph to projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -5,6 +5,11 @@
 
 <h1>Projects</h1>
 
+<p>
+  Here listed is a selection of projects I have worked on in the past and which I'm proud of. It
+  features both professional and personal projects.
+</p>
+
 <ul>
   {#each projects as project}
     <ProjectListItem {project} />
